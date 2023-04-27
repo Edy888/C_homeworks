5cf0bfe53f006a30b6d3034b3d91fd46d4df2355
Eduard Soldatkin <solde@mail.ru>
2023-04-27 19:15:30 +0300

    HW_2

--- HW2/Program.cs
+++ HW2/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Prompt (string message)
+{
+    System.Console.Write(message);
+    int value = Console.ReadLine();
+    int results = Convert.ToInt32(Console.ReadLine());
+    return results;
+}
+
+int number = Prompt("Input 3 dig value: ");
+if (number < 100 || number >= 1000)
+{
+    Console.WriteLine("error");
+    return;
+}
+
+Console.WriteLine($" Enter value `{number}`");
+int secondRank = number / 10 % 10;
+
+Console.WriteLine($"DSecond number `{secondRank}`" );
