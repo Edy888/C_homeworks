78414348153b28bcd81c707b9db4ba28ec73d8b0
Eduard Soldatkin <solde@mail.ru>
2023-04-27 18:37:01 +0300

    add id

--- HW2/Program.cs
+++ HW2/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.____________________
+
+void Second_N (numN)
+{
+
+}
