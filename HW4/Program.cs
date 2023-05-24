8c929b1751cb83ec6a4048b6a6b4af94e2b8cda8
Eduard Soldatkin <solde@mail.ru>
2023-05-24 13:02:23 +0300

    Задача 2 в работе

--- HW4/Program.cs
+++ HW4/Program.cs
@@ -3,6 +3,7 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
+/*
 void NaturalVal (int numA, int numB)
 {
     int sumC = 1 ;
@@ -22,14 +23,40 @@ int nB = Convert.ToInt32(Console.ReadLine()) ;
 
 NaturalVal (nA, nB);
 
-// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+*/
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе////////////////////////////////////////////////////////////////////////////////////////////////
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
 
-void NumSum (int value_X) ;
-{
-    int save_n = 0 ;
-    
 
+void NumSum (int valueX)
+{
+    int whole = 1 ;
+    int remainder = 0 ;
+    int save_n = 0 ;
+    while (whole != 0)
+    {
+        whole = valueX / 10;
+        remainder = valueX % 10 ;
+        save_n = remainder + save_n ;
+    }
+Console.WriteLine(save_n);
 }
+
+Console.WriteLine ("Enter your number_X : ");
+int NumX = Convert.ToInt32(Console.ReadLine());
+
+NumSum(NumX);
+
+/*
+int x = 224 ;
+int y = 5 ;
+int whole = x / y ; 
+int rema = x % y ;
+
+Console.WriteLine($"Whole is {whole}");
+Console.WriteLine($"Rem is {rema}");
+*/
