4dd0a4939281fbd3bb273b073863cf59cb770cc4
Eduard Soldatkin <solde@mail.ru>
2023-06-09 10:47:09 +0300

    add code

--- HW5/Program.cs
+++ HW5/Program.cs
@@ -1,22 +1,20 @@
 ﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве./////
 // [345, 897, 568, 234] -> 2
 
-/*
-int [] CreatePosRandom (int size, int min, int max)
+/* [] CreatePosRandom  size, min, max)
 {
-    int [] arPositive = new int [size] ;
-    for (int i = 0; i < size; i++)
+ [] arPositive = new [size] ;
+    for  i = 0; i < size; i++)
     {
         arPositive [i] = new Random().Next(min,max) ;        
     }
     return(arPositive) ;
 }
-
-int arrayEvenNum (int [] arrayCurrent)
+ arrayEvenNum  [] arrayCurrent)
 {
-    int count = 0 ;
+ count = 0 ;
 
-    for (int s = 0; s < arrayCurrent.Length; s++)
+    for  s = 0; s < arrayCurrent.Length; s++)
     {
         if (arrayCurrent [s] % 2 == 0)
         count ++ ;
@@ -24,19 +22,15 @@ int arrayEvenNum (int [] arrayCurrent)
     return(count);
 }
 
-void arrShow (int [] CreateAr)
+void arrShow  [] CreateAr)
 {
-    for (int h = 0; h < CreateAr.Length; h++)
+    for  h = 0; h < CreateAr.Length; h++)
     {
         Console.Write(CreateAr [h] + " ");
     }
 }
-
-int size = 200 ;
-int min = 100 ;
-int max = 999 ;
-
-int [] ArPosF = CreatePosRandom(size,min,max) ;
+ size = 200 ; min = 100 ; max = 999 ;
+ [] ArPosF = CreatePosRandom(size,min,max) ;
 arrShow (ArPosF) ;
 Console.WriteLine();
 Console.WriteLine();
@@ -48,29 +42,27 @@ Console.WriteLine($"Your number of positive values are {arrayEvenNum(ArPosF)}") 
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
-/*
-int [] ArrayCreater (int size, int min, int max) 
+/* [] ArrayCreater  size, min, max) 
 {
-    int [] ArrIndex = new int [size] ;
-    for (int i = 0; i < size; i++)
+ [] ArrIndex = new [size] ;
+    for  i = 0; i < size; i++)
     {
         ArrIndex [i] = new Random().Next(min, max);
     }
     return(ArrIndex);
 }
 
-void ShowArrInd (int [] ArrInd)
+void ShowArrInd  [] ArrInd)
 {
-    for (int s = 0; s < ArrInd.Length; s++)
+    for  s = 0; s < ArrInd.Length; s++)
     {
         Console.Write(ArrInd [s] + " ");
     }
 }
-
-int ArrSort (int [] ArrCurrent)
+ ArrSort  [] ArrCurrent)
 {
-    int sum = 0 ;
-    for (int j = 1; j < ArrCurrent.Length; j+=2)
+ sum = 0 ;
+    for  j = 1; j < ArrCurrent.Length; j+=2)
     {
         Console.Write(ArrCurrent [j] + " ") ;
         sum += ArrCurrent [j];
@@ -79,21 +71,16 @@ int ArrSort (int [] ArrCurrent)
    
 }
 
-Console.WriteLine("Enter size of your array: ") ;
-int size = Convert.ToInt32(Console.ReadLine());
-int min = 0 ;
-int max = 999 ;
+Console.WriteLine("Enter size of your array: ") ; size = Convert.T32(Console.ReadLine()); min = 0 ; max = 999 ;
 
 Console.WriteLine();
-
-int [] Arr = ArrayCreater(size, min, max);
+ [] Arr = ArrayCreater(size, min, max);
 Console.WriteLine("Your generated array is:");
 ShowArrInd(Arr);
 
 Console.WriteLine();
 Console.WriteLine();
-Console.WriteLine("Number which has ODD indexes: ");
-int summary = ArrSort(Arr) ; 
+Console.WriteLine("Number which has ODD indexes: "); summary = ArrSort(Arr) ; 
 
 Console.WriteLine();
 Console.WriteLine();
@@ -104,10 +91,10 @@ Console.WriteLine ($"Sum of Numbers which has ODD indexes is equal:   {summary}"
 // [3 7 22 2 78] -> 76
 
 /*
-double [] ArrayCreaDouble (int size)
+double [] ArrayCreaDouble  size)
 {
     double [] ArrIndex = new double [size] ;
-    for (int i = 0; i < size; i++)
+    for  i = 0; i < size; i++)
     {
         ArrIndex [i] = new Random().NextDouble();
     }
@@ -116,7 +103,7 @@ double [] ArrayCreaDouble (int size)
 
 void ShowArrInd (double [] ArrInd)
 {
-    for (int s = 0; s < ArrInd.Length; s++)
+    for  s = 0; s < ArrInd.Length; s++)
     {
         Console.Write($"{ArrInd [s]:f2}" + " ");
     }
@@ -126,13 +113,16 @@ void arrFinder (double [] ArrD)
 {
     double min = 0 ;
     double max = 0 ;
-    for (int x = 0; x < ArrD.Length; x++)
+    for  x = 0; x < ArrD.Length; x++)
     {
         if (ArrD [x] > max)
         {
             max = ArrD [x];
         }
-        else min = ArrD [x] ;
+        if (ArrD [x] < max)
+        {
+            min = ArrD [x] ;
+        }
     }
     Console.WriteLine() ;
     Console.WriteLine($"1. min is : {min:f2}") ;
@@ -141,8 +131,7 @@ void arrFinder (double [] ArrD)
     Console.WriteLine($"Result: max - min = {(max - min):f2}") ;
 }
 
-Console.WriteLine("Enter size of your array: ") ;
-int size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter size of your array: ") ; size = Convert.T32(Console.ReadLine());
 
 double [] ArrDouble = ArrayCreaDouble(size);
 
@@ -153,3 +142,139 @@ Console.WriteLine() ;
 arrFinder(ArrDouble) ;
 Console.WriteLine() ;
 */
+
+// Напишите программу, которая перевернет одномерный массив.
+
+/* [] ArCrea  size, min, max)
+{
+ [] ArrayNew = new [size] ;
+    for  i = 0; i < size; i++)
+    {
+        ArrayNew [i] = new Random().Next(min, max + 1) ;
+    }
+    return(ArrayNew) ;
+}
+
+void ShowArrInd  [] ArrInd)
+{
+    for  s = 0; s < ArrInd.Length; s++)
+    {
+        Console.Write(ArrInd [s] + " ");
+    }
+}
+ [] ArChange  [] Ar3)
+{
+ Length = Ar3.Length ;
+    for  i = 0; i < Ar3.Length/2; i++)
+    {
+     temp = 0 ;
+        temp = Ar3 [i] ;
+        Ar3[i] = Ar3[Length - 1 - i];
+        Ar3[Length - 1 - i] = temp ;
+    }
+
+return(Ar3) ;
+}
+
+
+Console.WriteLine("Enter size of your array: ") ; size = Convert.T32(Console.ReadLine()); min = 1 ; max = 99 ;
+ [] Array = ArCrea(size, min, max);
+ShowArrInd(Array) ;
+Console.WriteLine();
+Console.WriteLine(); [] MasNew = ArChange(Array);
+ShowArrInd(MasNew) ;
+*/
+
+// Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые 2 числа ФибоначчиЖ а и b (пользователь). 
+// 0 1 1 2 3 5 8 13
+// a b a+b=c b+c=d c+d=e
+
+// 1. Запросить N, a and b
+// 2. 
+
+
+/* [] ArCreate  count, nA, nB)
+{
+ [] arFibonachi = new [count];
+    arFibonachi [0] = nA ;
+    arFibonachi [1] = nB ;
+    for  i = 2; i < count; i++)
+    {
+        arFibonachi [i] = arFibonachi [i-2] + arFibonachi [i-1] ;
+    }
+    return (arFibonachi);
+}
+
+void arShow ( [] ar8)
+{
+    for  f = 0; f < ar8.Length; f++)
+    {
+        Console.Write(ar8[f] + " " );
+    }
+}
+
+Console.WriteLine("Enter your N: "); countN = Convert.T32(Console.ReadLine());
+
+Console.WriteLine("Enter your a: "); numA = Convert.T32(Console.ReadLine());
+
+Console.WriteLine("Enter your b: "); numB = Convert.T32(Console.ReadLine());
+ [] ArrC = ArCreate(countN, numA, numB);
+Console.WriteLine();
+arShow(ArrC);
+*/
+
+// Задача про треугольник
+/*
+string BinarSys (int Num)
+{
+    string binRes = String.Empty;
+    while (Num != 0)
+    {
+        binRes = Num % 2 + binRes ;
+        Console.WriteLine($"двоичное число (остаток от деления) = {binRes}");
+        Num /= 2 ;
+        Console.WriteLine($"(целочисленное деление) =  {Num}");
+    }
+    return(binRes);
+}
+
+Console.WriteLine("Enter your number:  ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string binresult = BinarSys(num);
+Console.WriteLine ($"Your number {num} is equal in binare system {binresult}");
+*/
+
+void Calculator (double a1, double b1, double a2, double b2)
+{
+   
+    if (a1 == a2)
+    {
+        Console.WriteLine("Your Lines are parallel");
+    }
+    else
+    {
+        double  x = (b2 - b1)/(a1-a2) ;
+        double y = a1 * x + b1;
+        Console.WriteLine($"Your coordinates x;y = {x:f3} ; {y:f3}");
+
+    }
+    if (a1 == a2 & b1 == b2)
+    {
+        Console.WriteLine("Your lines are coinside!!!");
+    }
+}
+
+Console.WriteLine("Enter your a1 (slope1):  ");
+double a1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter your b1 (intersept1):  ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter your a2 (slope2):  ");
+double a2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter your b2 (intersept2):  ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+Calculator(a1,b1,a2,b2);
---
+++ HW8/HW8.cs
@@ -0,0 +1,172 @@
+﻿// ЗАДАЧА 54: Задайте двумерный массив и напишите программу, которая упорядочет по убыванию элементы каждой строки двумерного массива._____________________________
+
+/*
+Console.WriteLine("Enter count of Rows:  ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter count of Columns:  ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter MIN value:  ");
+int minV = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter MAX value:  ");
+int maxV = Convert.ToInt32(Console.ReadLine());
+
+
+int [,] CreateDataFrame (int rows, int columns, int MIN, int MAX)
+{
+    int [,] createdDF = new int [rows,columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            createdDF [i,j] = new Random().Next(minV,maxV);
+        }
+    }
+    return createdDF;
+}
+
+void ShowDF (int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int [,]  ChangeDF (int [,] array)
+{
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns - 1; j++)
+        {
+            if (array[i,j] < array[i,j+1])
+            {
+                    int temp = array[i,j];
+                    array[i,j] = array[i, j + 1];
+                    array[i, j + 1] = temp;
+            }
+            
+        }
+    }
+    return array;
+}
+
+
+int [,] createDF = CreateDataFrame(rows,columns,minV, maxV);
+Console.WriteLine();
+Console.WriteLine("CreatedDF:");
+ShowDF(createDF);
+
+int [,] changeDF = ChangeDF(createDF);
+Console.WriteLine();
+Console.WriteLine("new chagedDF:");
+ShowDF(changeDF);
+*/
+//_____________________________________________________________________________________________________________________________________________________________
+
+// Задача 56: Создайте прямоугольный массив, и напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+/*
+Console.WriteLine("Enter count of Rows (количество строк должно быть равно количеству столбцов!!!):  ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter count of Columns (количество столбцов должно быть равно количеству строк!!!):  ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter MIN value:  ");
+int minV = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter MAX value:  ");
+int maxV = Convert.ToInt32(Console.ReadLine());
+
+
+int [,] CreateDataFrame (int rows, int columns, int MIN, int MAX)
+{
+    int [,] createdDF = new int [rows,columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            createdDF [i,j] = new Random().Next(minV,maxV);
+        }
+    }
+    return createdDF;
+}
+
+void ShowDF (int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void CalcMinSumRow (int [,] arrayDF)
+{
+    int rows = arrayDF.GetLength(0);
+    int columns = arrayDF.GetLength(1);
+   
+
+    int [] sumRows = new int [arrayDF.GetLength(0)];
+
+    for (int i = 0; i < rows; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < columns; j++)
+        {
+            sum += arrayDF[i,j];          
+        }
+         sumRows[i] = sum;
+        
+        Console.Write(sumRows[i] + " ");       
+    }
+    Console.WriteLine();
+
+    
+    int minSum = sumRows[0];
+    int minInd = 0;
+    for (int k = 1; k < sumRows.Length; k ++)
+    {
+        if( sumRows[k] < minSum )
+        {
+            minSum = sumRows[k];
+            minInd = k;
+        }    
+    }
+    Console.WriteLine($"Minimal sum equal {minSum} and it's row with index i = {minInd}");
+}
+
+if (rows == columns)
+{
+int [,] createDF = CreateDataFrame(rows,columns,minV, maxV);
+Console.WriteLine();
+Console.WriteLine("CreatedDF:");
+ShowDF(createDF);
+
+CalcMinSumRow(createDF);
+}
+else
+{
+  Console.WriteLine ("Your count of rows and columns arn't equal, enter valid data please!");
+}
+*/
+//______________________________________________________________________________________________________________________________________________________________
+
+// Задача 58. Задайте 2-е матрицы. Напишите программу, которая будет находить произведения 2-х матриц.
